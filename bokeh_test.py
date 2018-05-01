f479430829c0574b157be5bb8fee2179268fedc0
Athira Das <athiradas.mec@gmail.com>
2018-05-01 10:58:57 -0500

    Add files via upload

---
+++ bokeh_test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.special
+
+from bokeh.plotting import figure, show, output_file, vplot
+
+p1 = figure(title="Normal Distribution (μ=0, σ=0.5)",tools="save",
+            background_fill_color="#E8DDCB")
+
+mu, sigma = 0, 0.5
+
+measured = np.random.normal(mu, sigma, 1000)
+hist, edges = np.histogram(measured, density=True, bins=50)
+
+x = np.linspace(-2, 2, 1000)
+pdf = 1/(sigma * np.sqrt(2*np.pi)) * np.exp(-(x-mu)**2 / (2*sigma**2))
+cdf = (1+scipy.special.erf((x-mu)/np.sqrt(2*sigma**2)))/2
+
+p1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+        fill_color="#036564", line_color="#033649")
+p1.line(x, pdf, line_color="#D95B43", line_width=8, alpha=0.7, legend="PDF")
+p1.line(x, cdf, line_color="white", line_width=2, alpha=0.7, legend="CDF")
+
+p1.legend.location = "top_left"
+p1.xaxis.axis_label = 'x'
+p1.yaxis.axis_label = 'Pr(x)'
+
+output_file('histogram.html', title="histogram.py example")
+
+show(p1)
+
